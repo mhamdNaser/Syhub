d34e6cf2407be17dcd9b7fd72f5d5c489b046cd5
mhamdNaser <mhamd.naser.edden@gmail.com>
2025-08-14 02:08:58 +0300

    readme

--- assets/product-variant-switcher.js
+++ assets/product-variant-switcher.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
       colorLabel.textContent = selectedVariant.title; 
     }
 
-    thumbnailContainer.className('border-[#c42764]')
+    thumbnailContainer.classList.add('border-[#c42764]')
 
     // تحديث الصورة من الميديا المخفية
     if (selectedVariant.featured_media && selectedVariant.featured_media.id) {
