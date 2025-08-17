f2eef8e16e41dec904e44b3f220221c9cdd6c239
unknown <mhamd.naser.edden@gmail.com>
2025-08-17 18:14:11 +0300

    product

--- assets/product-variant-switcher.js
+++ assets/product-variant-switcher.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
   const product = JSON.parse(document.getElementById("product-data").textContent);
-  console.log(product.variants); // كل بيانات البرودكت
+  console.log(product.variants.first); // كل بيانات البرودكت
 
 
   function updateVariant(optionValues) {
