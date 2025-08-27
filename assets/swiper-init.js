9e59aeb2d82e5b3c2a40a3fdc58a25fe438fbe4a
mhamdNaser <mhamd.naser.edden@gmail.com>
2025-08-27 13:00:28 +0300

    menu

--- assets/swiper-init.js
+++ assets/swiper-init.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
           spaceBetween: 5,
         },
         768: {
-          slidesPerView: slidesPerView,
+          slidesPerView: slidesPerView -1,
           spaceBetween: 10,
         },
         1024: {
