809b233d92963fa64bd442e2876bc185754aa204
mhamdNaser <mhamd.naser.edden@gmail.com>
2025-09-01 12:21:33 +0300

    menu

--- assets/plugin-swiper-init.js
+++ assets/plugin-swiper-init.js
@@ -1,21 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-  
+
   const sliders = Array.from(document.querySelectorAll("[class*='mySwiper-']")).map(swiperContainer => {
     const classList = swiperContainer.className.split(" ");
     const mySwiperClass = classList.find(c => c.startsWith("mySwiper-"));
-    
+
     const nextBtn = "." + swiperContainer.dataset.nextBtn;
     const prevBtn = "." + swiperContainer.dataset.prevBtn;
 
     const slidesPerView = parseInt(swiperContainer.dataset.slidesPerView) || 2;
     const slidesPerViewMobile = parseInt(swiperContainer.dataset.slidesPerViewMobile) || 2;
 
+    const autoplayEnabled = swiperContainer.dataset.autoplay === "true";
+    const autoplayDelay = parseInt(swiperContainer.dataset.autoplayDelay) || 2500;
+
     return {
       container: swiperContainer,
       nextBtn,
       prevBtn,
       slidesPerView,
-      slidesPerViewMobile
+      slidesPerViewMobile,
+      autoplayEnabled,
+      autoplayDelay
     };
   });
 
@@ -24,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
       new Swiper(slider.container, {
         slidesPerView: slider.slidesPerView,
         spaceBetween: 10,
-        loop: false,
+        loop: true,
         navigation: {
           nextEl: slider.nextBtn,
           prevEl: slider.prevBtn
@@ -46,6 +51,14 @@ document.addEventListener("DOMContentLoaded", function () {
             slidesPerView: slider.slidesPerView,
             spaceBetween: 20,
           },
+        },
+        autoplay: slider.autoplayEnabled ? {
+          delay: slider.autoplayDelay,
+          disableOnInteraction: false
+        } : false,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
         },
       });
     });
