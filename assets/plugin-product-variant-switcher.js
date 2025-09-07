879a54ea88af16f9f3041e8178d7d70442f41579
unknown <mhamd.naser.edden@gmail.com>
2025-09-07 18:39:24 +0300

    product

--- assets/plugin-product-variant-switcher.js
+++ assets/plugin-product-variant-switcher.js
@@ -1,16 +1,14 @@
-
-document.addEventListener("DOMContentLoaded", function () {
+function initVariantSelector(sectionId) {
   var variantData = [];
   const scriptTag = document.querySelector('variant-selector script[type="application/json"]');
   const colorButtons = document.querySelectorAll(".color-swatch");
   const selects = document.querySelectorAll('variant-selector select');
   const mainImageContainer = document.getElementById("main-product-image");
   const mediaStorage = document.getElementById("all-variant-media");
-  const priceContainer = document.getElementById("price-{{ section.id }}");
+  const priceContainer = document.getElementById(`price-${sectionId}`);
   const hiddenInput = document.querySelector('#product-form input[name="id"]');
   const colorLabel = document.getElementById("variantLabel");
   const sku = document.getElementById("container-sku");
-
 
   if (scriptTag) {
     variantData = JSON.parse(scriptTag.textContent);
@@ -18,18 +16,15 @@ document.addEventListener("DOMContentLoaded", function () {
     console.warn("⚠️ not avaialble script[type='application/json'] داخل variant-selector");
   }
 
-
   function updateVariant(optionValues) {
     const selectedVariant = variantData.find(v => {
       return v.options.every((opt, i) => opt === optionValues[i]);
     });
 
-
     if (!selectedVariant) return;
 
     if (sku) {
-      sku.textContent = ''
-      sku.textContent = selectedVariant.sku
+      sku.textContent = selectedVariant.sku || "";
     }
 
     // إزالة البوردر من كل الثامبنيلز
@@ -43,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (thumbnailContainer) {
       thumbnailContainer.classList.add('border', 'border-[#c42764]');
 
-      // for move swiper to select variant slider
+      // تحريك السويبر للصورة المختارة
       if (window.thumbnailsSwiper) {
         const slideIndex = Array.from(thumbnailsSwiper.slides).findIndex(slide =>
           slide.contains(thumbnailContainer)
@@ -58,32 +53,29 @@ document.addEventListener("DOMContentLoaded", function () {
       colorLabel.textContent = selectedVariant.title;
     }
 
-
-
-    // تحديث الصورة من الميديا المخفية
+    // تحديث الصورة
     if (selectedVariant.featured_media && selectedVariant.featured_media.id) {
       const mediaId = selectedVariant.featured_media.id;
       const mediaHTML = mediaStorage.querySelector(`[data-media-id="${mediaId}"]`);
       if (mediaHTML) {
         mainImageContainer.innerHTML = mediaHTML.innerHTML;
         mainImageContainer.setAttribute("data-current-media-id", mediaId);
-
       }
     }
 
     // تحديث السعر
     if (priceContainer) {
       priceContainer.innerHTML = `
-      <span class="text-base text-gray-500 line-through">
-        ${selectedVariant.compare_at_price ? Shopify.formatMoney(selectedVariant.compare_at_price) : ""}
-      </span>
-      <span class="text-lg text-gray-900">${Shopify.formatMoney(selectedVariant.price)}</span>
-      ${selectedVariant.price < selectedVariant.compare_at_price ? '<span class="px-5 py-1 text-sm font-bold bg-red-500 rounded-full text-white mx-4">Sale</span>' : ''}
-    `;
+        <span class="text-base text-gray-500 line-through">
+          ${selectedVariant.compare_at_price ? Shopify.formatMoney(selectedVariant.compare_at_price) : ""}
+        </span>
+        <span class="text-lg text-gray-900">${Shopify.formatMoney(selectedVariant.price)}</span>
+        ${selectedVariant.price < selectedVariant.compare_at_price ? '<span class="px-5 py-1 text-sm font-bold bg-red-500 rounded-full text-white mx-4">Sale</span>' : ''}
+      `;
     }
 
     // تحديث ID الفارينت
-    hiddenInput.value = selectedVariant.id;
+    if (hiddenInput) hiddenInput.value = selectedVariant.id;
   }
 
   // عند الضغط على زر لون
@@ -102,7 +94,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // عند عمل hover على زر اللون
+  // عند hover على اللون
   colorButtons.forEach(btn => {
     btn.addEventListener("mouseenter", function () {
       const allcolorButtons = document.querySelectorAll('.color-swatch');
@@ -117,7 +109,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // عند تغيير أي قائمة
+  // عند تغيير أي select
   selects.forEach((sel) => {
     sel.addEventListener("change", function () {
       const selectsArray = Array.from(selects).map(s => s.value);
@@ -125,6 +117,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+  // عند الضغط على الثامبنيل
   const thumbnailButtons = document.querySelectorAll('[class^="thumbnail-container_"]');
   thumbnailButtons.forEach(btn => {
     btn.addEventListener("click", function () {
@@ -140,4 +133,147 @@ document.addEventListener("DOMContentLoaded", function () {
       updateVariant(selectsArray);
     });
   });
-});
+}
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var variantData = [];
+//   const scriptTag = document.querySelector('variant-selector script[type="application/json"]');
+//   const colorButtons = document.querySelectorAll(".color-swatch");
+//   const selects = document.querySelectorAll('variant-selector select');
+//   const mainImageContainer = document.getElementById("main-product-image");
+//   const mediaStorage = document.getElementById("all-variant-media");
+//   const priceContainer = document.getElementById("price-{{ section.id }}");
+//   const hiddenInput = document.querySelector('#product-form input[name="id"]');
+//   const colorLabel = document.getElementById("variantLabel");
+//   const sku = document.getElementById("container-sku");
+
+
+//   if (scriptTag) {
+//     variantData = JSON.parse(scriptTag.textContent);
+//   } else {
+//     console.warn("⚠️ not avaialble script[type='application/json'] داخل variant-selector");
+//   }
+
+
+//   function updateVariant(optionValues) {
+//     const selectedVariant = variantData.find(v => {
+//       return v.options.every((opt, i) => opt === optionValues[i]);
+//     });
+
+
+//     if (!selectedVariant) return;
+
+//     if (sku) {
+//       sku.textContent = ''
+//       sku.textContent = selectedVariant.sku
+//     }
+
+//     // إزالة البوردر من كل الثامبنيلز
+//     const allThumbnails = document.querySelectorAll('[class^="thumbnail-container_"]');
+//     allThumbnails.forEach(thumb => {
+//       thumb.classList.remove('border', 'border-[#c42764]');
+//     });
+
+//     // إضافة البوردر للثامبنيل المختار
+//     const thumbnailContainer = document.querySelector(`.thumbnail-container_${selectedVariant.featured_media.id}`);
+//     if (thumbnailContainer) {
+//       thumbnailContainer.classList.add('border', 'border-[#c42764]');
+
+//       // for move swiper to select variant slider
+//       if (window.thumbnailsSwiper) {
+//         const slideIndex = Array.from(thumbnailsSwiper.slides).findIndex(slide =>
+//           slide.contains(thumbnailContainer)
+//         );
+//         if (slideIndex >= 0) {
+//           thumbnailsSwiper.slideTo(slideIndex);
+//         }
+//       }
+//     }
+
+//     if (colorLabel) {
+//       colorLabel.textContent = selectedVariant.title;
+//     }
+
+
+
+//     // تحديث الصورة من الميديا المخفية
+//     if (selectedVariant.featured_media && selectedVariant.featured_media.id) {
+//       const mediaId = selectedVariant.featured_media.id;
+//       const mediaHTML = mediaStorage.querySelector(`[data-media-id="${mediaId}"]`);
+//       if (mediaHTML) {
+//         mainImageContainer.innerHTML = mediaHTML.innerHTML;
+//         mainImageContainer.setAttribute("data-current-media-id", mediaId);
+
+//       }
+//     }
+
+//     // تحديث السعر
+//     if (priceContainer) {
+//       priceContainer.innerHTML = `
+//       <span class="text-base text-gray-500 line-through">
+//         ${selectedVariant.compare_at_price ? Shopify.formatMoney(selectedVariant.compare_at_price) : ""}
+//       </span>
+//       <span class="text-lg text-gray-900">${Shopify.formatMoney(selectedVariant.price)}</span>
+//       ${selectedVariant.price < selectedVariant.compare_at_price ? '<span class="px-5 py-1 text-sm font-bold bg-red-500 rounded-full text-white mx-4">Sale</span>' : ''}
+//     `;
+//     }
+
+//     // تحديث ID الفارينت
+//     hiddenInput.value = selectedVariant.id;
+//   }
+
+//   // عند الضغط على زر لون
+//   colorButtons.forEach(btn => {
+//     btn.addEventListener("click", function () {
+//       const allcolorButtons = document.querySelectorAll('.color-swatch');
+//       allcolorButtons.forEach(btnbor => {
+//         btnbor.classList.remove('border', 'border-[#c42764]');
+//       });
+//       const optionIndex = parseInt(btn.dataset.optionIndex);
+//       const selectsArray = Array.from(selects).map(s => s.value);
+//       selectsArray[optionIndex] = btn.dataset.value;
+//       selects[optionIndex].value = btn.dataset.value;
+//       btn.classList.add('border', 'border-[#c42764]');
+//       updateVariant(selectsArray);
+//     });
+//   });
+
+//   // عند عمل hover على زر اللون
+//   colorButtons.forEach(btn => {
+//     btn.addEventListener("mouseenter", function () {
+//       const allcolorButtons = document.querySelectorAll('.color-swatch');
+//       allcolorButtons.forEach(btnbor => {
+//         btnbor.classList.remove('border', 'border-[#c42764]');
+//       });
+//       const optionIndex = parseInt(btn.dataset.optionIndex);
+//       const selectsArray = Array.from(selects).map(s => s.value);
+//       selectsArray[optionIndex] = btn.dataset.value;
+//       btn.classList.add('border', 'border-[#c42764]');
+//       updateVariant(selectsArray);
+//     });
+//   });
+
+//   // عند تغيير أي قائمة
+//   selects.forEach((sel) => {
+//     sel.addEventListener("change", function () {
+//       const selectsArray = Array.from(selects).map(s => s.value);
+//       updateVariant(selectsArray);
+//     });
+//   });
+
+//   const thumbnailButtons = document.querySelectorAll('[class^="thumbnail-container_"]');
+//   thumbnailButtons.forEach(btn => {
+//     btn.addEventListener("click", function () {
+//       const mediaId = parseInt(btn.getAttribute("class").match(/thumbnail-container_(\d+)/)[1]);
+//       const selectedVariant = variantData.find(v => v.featured_media && v.featured_media.id === mediaId);
+//       if (!selectedVariant) return;
+
+//       const selectsArray = Array.from(selects).map(s => s.value);
+//       selectedVariant.options.forEach((opt, i) => {
+//         selectsArray[i] = opt;
+//         selects[i].value = opt;
+//       });
+//       updateVariant(selectsArray);
+//     });
+//   });
+// });
